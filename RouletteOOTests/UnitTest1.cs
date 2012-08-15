645d3908f4b270eb1dd7202fd05c5bc956b335ef
Jeremie Smaga <jeremie@smaga.ch>
2012-08-15 04:12:53 +0200

    Added Unit Test Projects

---
+++ RouletteOOTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RouletteOOLib;
+
+namespace RouletteOOTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NumberTest()
+        {
+            Number zero = new Number(0);
+            Assert.AreEqual(NumberColor.None, zero.Color);
+            Number one = new Number(1);
+            Assert.AreEqual(NumberColor.Red, one.Color);
+            Number two = new Number(2);
+            Assert.AreEqual(NumberColor.Black, two.Color);
+        }
+    }
+}
